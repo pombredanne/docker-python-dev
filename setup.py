d2b048dfc9ca8775939414381ae5dfbb91e7fde6
Mangled Deutz <olivier@webitup.fr>
2014-05-21 15:48:08 +0200

    Fix packaging

--- setup.py
+++ setup.py
@@ -34,6 +34,10 @@ if ver[0] == 2:
         # Python 2.6 requires additional libraries
         requirements.insert(0, 'argparse>=1.2.1')
 
+packages = [
+    'docker_registry',
+    'docker_registry.tools',
+    'docker_registry.tools.driver']
 package_data = {'scaffold': ['../scaffold/*']}
 
 setuptools.setup(
@@ -48,7 +52,7 @@ setuptools.setup(
     download_url=tools.__download__,
     long_description=open('./README.md').read(),
     namespace_packages=['docker_registry'],
-    packages=['docker_registry', 'docker_registry.tools'],
+    packages=packages,
     package_data=package_data,
     classifiers=['Development Status :: 4 - Beta',
                  'Intended Audience :: Developers',
