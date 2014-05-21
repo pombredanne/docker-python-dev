4a44c786cd2ba0ada977d4f5a11f2da3f70e05b2
Mangled Deutz <olivier@webitup.fr>
2014-05-21 15:46:04 +0200

    WIP

--- setup.py
+++ setup.py
@@ -1,43 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Copyright (c) 2014 Docker.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 try:
     import setuptools
 except ImportError:
     import distutils.core as setuptools
 
+import sys
 
-__author__ = 'Mangled Deutz'
-__copyright__ = 'Copyright 2014'
-__credits__ = []
+import docker_registry.tools as tools
 
-__license__ = 'Apache 2.0'
-__version__ = '0.0.1'
-__maintainer__ = 'Mangled Deutz'
-__email__ = 'olivier@webitup.fr'
-__status__ = 'Production'
+requirements_txt = open('./requirements.txt')
+requirements = [line for line in requirements_txt]
 
-__title__ = 'docker-registry-tools'
-__build__ = 0x000000
+ver = sys.version_info
 
-__url__ = 'https://github.com/dmp42/docker-registry-tools'
-__description__ = 'Docker registry tools'
-d = 'https://github.com/dmp42/docker-registry-tools/archive/master.zip'
+if ver[0] == 2:
+    if ver[1] <= 6:
+        # Python 2.6 requires additional libraries
+        requirements.insert(0, 'argparse>=1.2.1')
+
+package_data = {'scaffold': ['../scaffold/*']}
 
 setuptools.setup(
-    name=__title__,
-    version=__version__,
-    author=__author__,
-    author_email=__email__,
-    maintainer=__maintainer__,
-    maintainer_email=__email__,
-    url=__url__,
-    description=__description__,
+    name=tools.__title__,
+    version=tools.__version__,
+    author=tools.__author__,
+    author_email=tools.__email__,
+    maintainer=tools.__maintainer__,
+    maintainer_email=tools.__email__,
+    url=tools.__url__,
+    description=tools.__description__,
+    download_url=tools.__download__,
     long_description=open('./README.md').read(),
-    download_url=d,
+    namespace_packages=['docker_registry'],
+    packages=['docker_registry', 'docker_registry.tools'],
+    package_data=package_data,
     classifiers=['Development Status :: 4 - Beta',
                  'Intended Audience :: Developers',
-                 'Programming Language :: Python',
                  'Programming Language :: Python :: 2.6',
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3.2',
@@ -49,12 +63,17 @@ setuptools.setup(
                  'License :: OSI Approved :: Apache Software License'],
     platforms=['Independent'],
     license=open('./LICENSE').read(),
-    namespace_packages=['docker_registry'],
-    packages=['docker_registry', 'docker_registry.tools'],
-    install_requires=open('./requirements.txt').read(),
     zip_safe=False,
-    tests_require="%s\n%s" % (
-        open('./tests/requirements-tests.txt').read(),
-        open('./tests/requirements-style.txt').read()),
-    test_suite='nose.collector'
+    test_suite='nose.collector',
+    entry_points={
+        'console_scripts': [
+            'drt-driver = docker_registry.tools.driver:main',
+        ]
+    },
+    install_requires=open('./requirements.txt').read(),
+    tests_require=open('./requirements-test.txt').read(),
+    extras_require={
+        'style': open('./requirements-style.txt').read(),
+        'test': open('./requirements-test.txt').read()
+    }
 )
